84cbfad8a47af87fd9d17f4fe948edccd301f9ca
Emma22Tsai <raypontsai@gmail.com>
2014-11-30 22:18:59 +0800

    Create IF_ELSE.py

---
+++ IF_ELSE.py
@@ -0,0 +1,19 @@
+#======================================================================================
+#
+#                       EDIT HISTORY FOR MODULE
+#
+#   This section contains comments describing changes made to the module.
+#   Notice that changes are listed in reverse chronological order.
+#
+# when       who     version    what, where, why
+# --------   ------  -------    -------------------------------------------------------
+# 11-30-14   Emma     1.0.0     if-else
+#
+#
+#======================================================================================
+password = raw_input("Please enter the password:")
+
+if password == "123":
+  print "Access grant"
+else:
+  print "Access denied"
