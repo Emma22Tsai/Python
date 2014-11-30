1c58c2cf45110c35ad626a1da97c75a87966647a
Emma22Tsai <raypontsai@gmail.com>
2014-11-30 12:16:40 +0800

    Create Hello_world.py

---
+++ Hello_world.py
@@ -0,0 +1,17 @@
+#======================================================================================
+#
+#                       EDIT HISTORY FOR MODULE
+#
+#   This section contains comments describing changes made to the module.
+#   Notice that changes are listed in reverse chronological order.
+#
+# when       who     version    what, where, why
+# --------   ------  -------    -------------------------------------------------------
+# 11-30-14   Emma     1.0.0     Hello world!
+#
+#
+#======================================================================================
+
+import re, os, glob, fnmatch, array
+
+print "Hello world"
