e29dedab1428b17b708748ab3fb2e554ba872919
Emma22Tsai <raypontsai@gmail.com>
2014-11-30 13:33:15 +0800

    Create Print.py

---
+++ Print.py
@@ -0,0 +1,29 @@
+#======================================================================================
+#
+#                       EDIT HISTORY FOR MODULE
+#
+#   This section contains comments describing changes made to the module.
+#   Notice that changes are listed in reverse chronological order.
+#
+# when       who     version    what, where, why
+# --------   ------  -------    -------------------------------------------------------
+# 11-30-14   Emma     1.0.0     Exercise of "print"
+#
+#
+#======================================================================================
+
+#Python script
+
+#Load a library module
+import sys              
+print(sys.platform)
+
+#Raise 2 to a power
+print(2**6)
+
+#String repetition
+x = 'Mini!'
+print(x*8)
+
+#Concatenate for string
+print('Hello ' + 'world')
